8e3ecfcbb3eee75953db46cb5377f04eed311716
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-01-17 22:37:08 +0100

    updated: solved leetcode problem concatenation of arrays

---
+++ solutions in Python/concatenation-of-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+  def getConcatenation(self, nums):
+    newArray = nums + nums
+
+    return newArray
+
+newArrayObject = Solution()
+print(newArrayObject.getConcatenation([1,2,3]))
+
+        
