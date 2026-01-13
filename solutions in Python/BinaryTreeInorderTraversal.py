c99a951b3a3cae764c7e174dc9eeae2eb36e3333
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-01-13 14:00:49 +0100

    update: progress on solution

--- solutions in Python/BinaryTreeInorderTraversal.py
+++ solutions in Python/BinaryTreeInorderTraversal.py
@@ -6,18 +6,32 @@ class TreeNode(object):
 
 class Solution(object):
     def inorderTraversal(self, root):
-        stack = []
         i = 0
+        stack = [0]
+        currentInteration = []
         
-        if (2 * i) + 1:
-            currentLeftmostChild = root[(2 * i) + 1]
-        else:
-            if i == 0:
-                currentLeftmostChild = root[i]
+        for j in range(len(root)):
+            if root[(2 * i) + 1]:
+                i = (2 * i) + 1
+                stack.append(i)
+            elif root[(2 * i) + 2]:
+                currentInteration.append(root[i])
+                root[i] = None
+                stack.pop()
+
+                i = (2 * i) + 2
+                stack.append(i)
             else:
-                stack.append(currentLeftmostChild)
-                if (i -  1) / 2 != 0:
-                    i = (i -  1) / 2
-                else:
-                    i = (2 * i) + 2
+                currentInteration.append(root[i])
+
+                root[i] = None
+                stack.pop()
+
         
+        return currentInteration
+    
+
+practice = Solution()
+
+root = [1,2,3,4,5,None,8,None,None,6,7,9]
+print(practice.inorderTraversal(root))
