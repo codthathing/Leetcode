69a988cca95b906931c0d763f480bcbb25526cca
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-05-20 19:48:16 +0100

    update: completed the leetcode problem

--- solutions in Python/BinaryTreeInorderTraversal.py
+++ solutions in Python/BinaryTreeInorderTraversal.py
@@ -1,37 +1,20 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution(object):
     def inorderTraversal(self, root):
-        i = 0
-        stack = [0]
-        currentInteration = []
+        res = []
         
-        for j in range(len(root)):
-            if root[(2 * i) + 1]:
-                i = (2 * i) + 1
-                stack.append(i)
-            elif root[(2 * i) + 2]:
-                currentInteration.append(root[i])
-                root[i] = None
-                stack.pop()
+        def helper(node):
+            if node is None:
+                return
+            
+            helper(node.left)
+            res.append(node.val)
+            helper(node.right)
 
-                i = (2 * i) + 2
-                stack.append(i)
-            else:
-                currentInteration.append(root[i])
-
-                root[i] = None
-                stack.pop()
-
-        
-        return currentInteration
+        helper(root)
+        return res
     
 
 practice = Solution()
 
-root = [1,2,3,4,5,None,8,None,None,6,7,9]
+root: list[int | None] = [1,2,3,4,5,None,8,None,None,6,7,9]
 print(practice.inorderTraversal(root))
