5a29aff2eb43e7e2759078bda143efbf1ff2b329
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-08-20 21:38:44 +0100

    Resolved a leetcode problem

--- concatenation-of-array.py
+++ concatenation-of-array.py
@@ -1,10 +1,15 @@
-class Solution(object):
-  def getConcatenation(self, nums):
-    newArray = nums + nums
+class Solution:
+  def getConcatenation(self, nums: list[int]) -> list[int]:
+    ans: list[int] = []
+    numsLength: int = len(nums)
 
-    return newArray
+    for i, n in enumerate(nums):
+      ans.insert(i, n)
+      ans.insert((i + numsLength), n)
+
+    return ans
 
 newArrayObject = Solution()
-print(newArrayObject.getConcatenation([1,2,3]))
+print(newArrayObject.getConcatenation([1,4,1,2]))
 
         
